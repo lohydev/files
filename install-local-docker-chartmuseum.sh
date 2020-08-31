73464fdc67c364d4fb197163a2c4be94fb66e398
HY Lo <hengyulo@yahoo.com>
2020-08-31 17:59:00 +0800

    can't do --rm with --restart=unless-stopped so removing --rm

--- install-local-docker-chartmuseum.sh
+++ install-local-docker-chartmuseum.sh
@@ -1,2 +1,2 @@
-sudo docker run --rm -d -p 5000:5000 --restart=unless-stopped registry:2
-sudo docker run --rm -d -p 8080:8080 --restart=unless-stopped -e STORAGE=local -e STORAGE_LOCAL_ROOTDIR=/charts -v $(pwd)/charts:/charts chartmuseum/chartmuseum
+sudo docker run -d -p 5000:5000 --restart=unless-stopped registry:2
+sudo docker run -d -p 8080:8080 --restart=unless-stopped -e STORAGE=local -e STORAGE_LOCAL_ROOTDIR=/charts -v $(pwd)/charts:/charts chartmuseum/chartmuseum
