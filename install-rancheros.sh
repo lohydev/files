090cd2236f87c1cfac0f6a69ae655ccb5e5f7193
HY Lo <hengyulo@yahoo.com>
2020-11-16 15:10:39 +0800

    removing password from RancherOS install script. didn't work

--- install-rancheros.sh
+++ install-rancheros.sh
@@ -1,4 +1,3 @@
 wget https://raw.githubusercontent.com/lohydev/files/master/rancheros-cloud-config.yml -O cloud-config.yml
 sed -i "s/my-rancheros-host/$1/" cloud-config.yml
-sed -i "s/my-rancheros-password/$2/" cloud-config.yml
 sudo ros install -c cloud-config.yml -d /dev/sda
