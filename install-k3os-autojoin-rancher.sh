6e0c0696e139a8c0251a9607cfbc4067f12d5bc0
HY Lo <hengyulo@yahoo.com>
2020-11-17 16:33:01 +0800

    re-adding password to RancherOS install via command line arg, tweaking k3os install to see if it'll work

--- install-k3os-autojoin-rancher.sh
+++ install-k3os-autojoin-rancher.sh
@@ -1,6 +1,5 @@
-wget https://raw.githubusercontent.com/lohydev/files/master/k3os-cloud-config.yml -O cloud-config.yml
-sed -i "s/my-k3os-host/$1/" cloud-config.yml
-sed -i "s/my-k3os-password/$2/" cloud-config.yml
-sed -i "s/my-rancher-server/$3/" cloud-config.yml
-sed -i "s/my-rancher-password/$4/" cloud-config.yml
-sudo k3os install -c cloud-config.yml -d /dev/sda
+wget https://raw.githubusercontent.com/lohydev/files/master/k3os-cloud-init.yml -O cloud-init.yml
+sed -i "s/my-k3os-host/$1/" cloud-init.yml
+sed -i "s/my-k3os-password/$2/" cloud-init.yml
+sed -i "s/my-rancher-server/$3/" cloud-init.yml
+sed -i "s/my-rancher-password/$4/" cloud-init.yml
--- install-rancheros.sh
+++ install-rancheros.sh
@@ -1,3 +1,3 @@
 wget https://raw.githubusercontent.com/lohydev/files/master/rancheros-cloud-config.yml -O cloud-config.yml
 sed -i "s/my-rancheros-host/$1/" cloud-config.yml
-sudo ros install -c cloud-config.yml -d /dev/sda
+sudo ros install -c cloud-config.yml -d /dev/sda --append "rancher.password=$2"
