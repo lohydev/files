a1dd79ddecfd566b7ca1a0ca5fc13894849b1738
HY Lo <hengyulo@gmail.com>
2021-06-17 03:55:52 +0800

    adding files to install k3os without automatically joining Rancher

--- install-k3os-autojoin-rancher.sh
+++ install-k3os-autojoin-rancher.sh
@@ -1,4 +1,4 @@
-wget https://raw.githubusercontent.com/lohydev/files/master/k3os-cloud-init.yml -O cloud-init.yml
+wget https://raw.githubusercontent.com/lohydev/files/master/k3os-cloud-init-join-rancher.yml -O cloud-init.yml
 sed -i "s/my-k3os-host/$1/" cloud-init.yml
 sed -i "s/my-k3os-password/$2/" cloud-init.yml
 sed -i "s/my-rancher-server/$3/" cloud-init.yml
