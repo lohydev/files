a358229ce3d7723391401dc5475a18339caaa4d7
HY Lo <hengyulo@gmail.com>
2021-06-18 18:34:21 +0800

    updating microk8s-rancher script for newer versions which no longer needs tiller

--- install-microk8s-rancher.sh
+++ install-microk8s-rancher.sh
@@ -2,17 +2,18 @@ sudo apt update
 sudo apt upgrade -y
 sudo snap install microk8s --classic --edge
 sleep 2m
-sudo microk8s.enable dns storage ingress helm
+sudo microk8s.enable dns storage ingress helm3
 sudo sh -c 'echo "--allow-privileged=true" >> /var/snap/microk8s/current/args/kube-apiserver'
 sudo systemctl restart snap.microk8s.daemon-apiserver.service
 sleep 2m
-sudo microk8s.kubectl create serviceaccount tiller --namespace kube-system
-sudo microk8s.kubectl create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admin --serviceaccount=kube-system:tiller
-sudo microk8s.helm init --service-account=tiller
-sudo microk8s.kubectl apply -f https://raw.githubusercontent.com/jetstack/cert-manager/release-0.9/deploy/manifests/00-crds.yaml
+sudo microk8s.helm3 repo add rancher-latest https://releases.rancher.com/server-charts/latest
+sudo microk8s.kubectl create namespace cattle-system
+sudo microk8s.kubectl apply -f https://github.com/jetstack/cert-manager/releases/download/v1.0.4/cert-manager.crds.yaml
 sudo microk8s.kubectl create namespace cert-manager
-sudo microk8s.kubectl label namespace cert-manager certmanager.k8s.io/disable-validation=true
+sudo microk8s.helm3 repo add jetstack https://charts.jetstack.io
+sudo microk8s.helm3 repo update
+sudo microk8s.helm3 install cert-manager jetstack/cert-manager --namespace cert-manager --version v1.0.4
 sleep 4m
-sudo microk8s.helm repo add jetstack https://charts.jetstack.io
-sudo microk8s.helm install --name cert-manager --namespace cert-manager --version v0.9.1 jetstack/cert-manager
-sudo microk8s.helm repo add rancher-stable https://releases.rancher.com/server-charts/stable
+rancherHostname="${HOSTNAME}.local"
+sudo microk8s.helm3 install rancher rancher-latest/rancher --namespace cattle-system --set hostname=$rancherHostname
+sudo echo "127.0.0.1  ${rancherHostname}" >> /etc/hosts
