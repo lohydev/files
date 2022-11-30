73d6ec3df6a2c2feeba9b081615581e079ea25cf
lohydev <69099322+lohydev@users.noreply.github.com>
2022-11-30 15:20:20 +0800

    Update install-rancheros.sh

--- install-rancheros.sh
+++ install-rancheros.sh
@@ -1,3 +1,5 @@
+# wget https://raw.githubusercontent.com/lohydev/files/master/install-rancheros.sh | bash -s my-rancher my-password
+
 wget https://raw.githubusercontent.com/lohydev/files/master/rancheros-cloud-config.yml -O cloud-config.yml
-sed -i "s/my-rancher/$1/" cloud-config.yml
+sed -i "s/my-rancher-host/$1/" cloud-config.yml
 sudo ros install -c cloud-config.yml -d /dev/sda --append "rancher.password=$2" --append "rancher.autologin=tty1"
