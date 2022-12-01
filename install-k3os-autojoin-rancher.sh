a76886b917db1f0772cfb53bcdfaa3d3e86c9636
lohydev <69099322+lohydev@users.noreply.github.com>
2022-12-01 17:09:11 +0800

    Delete install-k3os-autojoin-rancher.sh

--- install-k3os-autojoin-rancher.sh
+++
@@ -1,5 +0,0 @@
-wget https://raw.githubusercontent.com/lohydev/files/master/k3os-cloud-init-join-rancher.yml -O cloud-init.yml
-sed -i "s/my-k3os-host/$1/" cloud-init.yml
-sed -i "s/my-k3os-password/$2/" cloud-init.yml
-sed -i "s/my-rancher-server/$3/" cloud-init.yml
-sed -i "s/my-rancher-password/$4/" cloud-init.yml
