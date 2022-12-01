0aa9468bf9f98db057a6c750bf3fde31200ef8ba
lohydev <69099322+lohydev@users.noreply.github.com>
2022-12-01 17:01:26 +0800

    Update install-k3os-main-master.sh

--- install-k3os-main-master.sh
+++ install-k3os-main-master.sh
@@ -3,3 +3,4 @@
 wget https://raw.githubusercontent.com/lohydev/files/master/k3os-main-master-cloud-init.yml -O cloud-init.yml
 sed -i "s/my-k3os-host/$1/" cloud-init.yml
 sed -i "s/my-k3os-password/$2/" cloud-init.yml
+sudo k3os install
