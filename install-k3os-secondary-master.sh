5a6ed142d37898356f313563a0dc120a4f5519d0
lohydev <69099322+lohydev@users.noreply.github.com>
2022-12-02 20:47:42 +0800

    Update install-k3os-secondary-master.sh

--- install-k3os-secondary-master.sh
+++ install-k3os-secondary-master.sh
@@ -1,7 +1,6 @@
-# wget https://raw.githubusercontent.com/lohydev/files/master/install-k3os-secondary-master.sh | bash -s my-k3os-host my-k3os-password my-main-master
+# curl https://raw.githubusercontent.com/lohydev/files/master/install-k3os-secondary-master.sh | bash -s my-k3os-host my-k3os-password my-main-master
 
 wget https://raw.githubusercontent.com/lohydev/files/master/k3os-secondary-master-cloud-init.yml -O cloud-init.yml
 sed -i "s/my-k3os-host/$1/" cloud-init.yml
 sed -i "s/my-k3os-password/$2/" cloud-init.yml
 sed -i "s/my-main-master/$3/" cloud-init.yml
-sudo k3os install
